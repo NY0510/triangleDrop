9df4a1d797d1995a2786bd507f7a0441b330f567
obtuse@obtuse.kro.kr <obtuse@obtuse.kro.kr>
2022-08-19 20:17:50 +0900

    file split 1

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
+const { json } = require("express");
+
 const socket = io();
 
 const welcome = document.getElementById("welcome");
 const inRoom = document.getElementById("inRoom");
 
+const sendProgress = document.querySelector("progress#sendProgress");
+const receiveProgress = document.querySelector("progress#receiveProgress");
+
 let roomName;
 let myPeerConnection;
 let myDataChannel;
+let fileReader;
+
+let timestampStart;
+
+let receiveBuffer = [];
+let receivedSize = 0;
 
 inRoom.hidden = true;
 
@@ -95,14 +106,30 @@ function handleSendMessage(event) {
 
 function handleReceiveMessage(event) {
   console.log(event.data);
-  const message = JSON.parse(event.data);
-  console.log(message);
-  if (message.type === "chat") {
+  console.log(typeof event.data);
+  // console.log(message);
+  if (typeof event.data === "string") {
+    const message = JSON.parse(event.data);
+    if (message.type == "filesignal") {
+    }
     messageBlock.innerHTML += `<li>${message.value}</li>`;
-  } else if (message.type === "file") {
-    fetch(message.value)
-      .then((res) => res.blob())
-      .then(saveFile);
+  } else if (typeof event.data === "object") {
+    receiveBuffer.push(event.data);
+    receivedSize += event.data.byteLength;
+    receiveProgress.value = receivedSize;
+
+    const file = fileInput.files[0];
+
+    if (receivedSize === file.size) {
+      const blob = new Blob(receiveBuffer);
+      receiveBuffer = [];
+
+      saveFile(blob);
+
+      // const bitrate = Math.round(
+      //   (receivedSize * 8) / (new Date().getTime() - timestampStart)
+      // );
+    }
   }
 }
 
@@ -125,10 +152,50 @@ fileForm.addEventListener("submit", handleSendFile);
 
 function handleSendFile(event) {
   event.preventDefault();
-  const file = document.getElementById("fileInput");
-  const fileReader = new FileReader();
-  fileReader.onload = (event) => {
-    myDataChannel.send(`{"type": "file", "value": "${event.target.result}"}`);
-  };
-  fileReader.readAsDataURL(file.files[0]);
+  const file = document.getElementById("fileInput").files[0];
+  console.log(
+    `File is ${[file.name, file.size, file.type, file.lastModified].join(" ")}`
+  );
+
+  myDataChannel.send(
+    `{"type": "filesignal", "fileName": "${file.name}", "fileSize": ${file.size}, "fileType": "${file.type}", "fileLastModified": ${file.lastModified}}`
+  );
+
+  if (file.size === 0) {
+    alert("File is empty");
+    return;
+  }
+
+  sendProgress.max = file.size;
+  const chunkSize = 16384;
+
+  fileReader = new FileReader();
+  let offset = 0;
+
+  fileReader.addEventListener("error", (error) => {
+    alert("Error reading file");
+  });
+  fileReader.addEventListener("abort", (event) => {
+    alert("File reading aborted");
+  });
+  fileReader.addEventListener("load", (event) => {
+    console.log("FileRead.onload", event);
+    myDataChannel.send(event.target.result);
+    offset += event.target.result.byteLength;
+    console.log(`Sent ${offset} bytes`);
+    sendProgress.value = offset;
+    if (offset < file.size) {
+      readSlice(offset);
+    }
+  });
+
+  function readSlice(o) {
+    console.log("readSlice", o);
+    const slice = file.slice(offset, o + chunkSize);
+    fileReader.readAsArrayBuffer(slice);
+  }
+  readSlice(0);
+
+  // fileReader.onload = (event) => {};
+  // fileReader.readAsDataURL(file);
 }
