ce5ef843bd78d472999a0dae85df80340e313bdf
obtuse@obtuse.kro.kr <obtuse@obtuse.kro.kr>
2022-09-19 15:17:12 +0900

    file share target update v3.1

--- public/js/fileShare.js
+++ public/js/fileShare.js
@@ -1,10 +1,14 @@
-navigator.serviceWorker.addEventListener("message", function (event) {
-  if (searchParams.has("receiving-file-share")) {
-    // CODELAB: Add message event handler here.
-    // Handle file share.
-    const files = event.data.files;
-    document.write("Received file share: " + files[0].name);
-    console.log("Received files:", files);
-    // ...
-  }
-});
+window.onload = function () {
+  console.log("window.onload");
+  navigator.serviceWorker.addEventListener("message", function (event) {
+    document.querySelector("#sector1").write("message received");
+    if (searchParams.has("receiving-file-share")) {
+      // CODELAB: Add message event handler here.
+      // Handle file share.
+      const files = event.data.files;
+      document.write("Received file share: " + files[0].name);
+      console.log("Received files:", files);
+      // ...
+    }
+  });
+};
